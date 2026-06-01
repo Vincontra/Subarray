bbc4b359eb391d280a016d3b4da4bb18933d7cb3
vinay <vinaygadge2003@gmail.com>
2026-06-01 11:34:39 +0530

    IMP one logic added

--- SubarraySumEqualsK560.java
+++ SubarraySumEqualsK560.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class SubarraySumEqualsK560 {
      static class Solution {
+
+         //logic:
+         // for current index how many such subarrays may exist which has sum==k
+         // for that matter we need hashmap
+         // can be done in O(N^3 traversal+sum) and O(N^2 using traversal+prefix sum)
+         //DONE!!!!!!!
         public int subarraySum(int[] nums, int k) {
             HashMap<Integer,Integer>hm=new HashMap<>();
             int ans=0;
